cb2d526b8b798a830a23a47edd06309cbae5396f
Thomas Chobert <tchobert@made-f0Cr5s2.clusters.42paris.fr>
2024-09-19 19:58:19 +0200

    19/09

--- map_parser/map_lines_checkers.c
+++ map_parser/map_lines_checkers.c
@@ -6,13 +6,14 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 18:00:19 by tchobert          #+#    #+#             */
-/*   Updated: 2024/09/19 19:44:15 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/09/19 19:55:29 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-t_map_line_status	check_first_line(const char *first_line, t_map_data *map_data)
+t_map_line_status	check_first_line(const char *first_line,
+						t_map_data *map_data)
 {
 	if (first_line == NULL || is_full_wall(first_line) == false)
 		return (INVALID_LINE);
@@ -22,14 +23,16 @@ t_map_line_status	check_first_line(const char *first_line, t_map_data *map_data)
 
 t_map_line_status	check_last_line(const char *last_line, t_map_data *map_data)
 {
-	if (is_full_wall(last_line) == false || is_correct_size(last_line, map_data) == false)
+	if (is_full_wall(last_line) == false
+		|| is_correct_size(last_line, map_data) == false)
 		return (INVALID_LINE);
 	return (VALID_LINE);
 }
 
 t_map_line_status	check_line(const char *line, t_map_data *map_data)
 {
-	if ((is_correct_size(line, map_data) == false) || (is_surrounded_by_walls(line, map_data) == false))
+	if ((is_correct_size(line, map_data) == false)
+		|| (is_surrounded_by_walls(line, map_data) == false))
 	{
 		return (INVALID_LINE);
 	}
