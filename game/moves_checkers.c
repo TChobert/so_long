850609807d6196440b4bf704282cab2fddac6320
Thomas Chobert <tchobert@made-f0Dr6s1.clusters.42paris.fr>
2024-09-27 15:18:12 +0200

    27/09

--- game/moves_checkers.c
+++ game/moves_checkers.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_moves.c                                      :+:      :+:    :+:   */
+/*   moves_checkers.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:31:42 by tchobert          #+#    #+#             */
-/*   Updated: 2024/09/27 14:32:20 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:09:02 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_move_status	check_exit_move(t_game_data *game_data, unsigned int *character_ne
 	if (game_data->map_data.map_array[character_new_coords[0]][character_new_coords[1]] == EXIT_ITEM && game_data->map_data.items_values.collectibles_number == 0)
 	{
 		game_data->character_data.moves_counter+= 1;
+		game_data->map_data.items_values.exit_number -= 1;
 		return (EXIT_MOVE);
 	}
 	return (VALID_MOVE);
--- utils/close_game.c
+++ utils/close_game.c
@@ -6,28 +6,48 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:43:10 by tchobert          #+#    #+#             */
-/*   Updated: 2024/09/27 15:01:54 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:15:59 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void		display_end_message(t_game_data *game_data)
+{
+	if (game_data->map_data.items_values.collectibles_number >= 0 && game_data->map_data.items_values.exit_number > 0)
+	{
+		ft_printf("You made %d moves. See you !\n", game_data->character_data.moves_counter);
+	}
+	else if (game_data->map_data.items_values.collectibles_number == 0 && game_data->map_data.items_values.exit_number == 0)
+	{
+		ft_printf("You made %d moves. Well done !\n", game_data->character_data.moves_counter);
+	}
+}
+
 static	void	destroy_game_images(t_game_data *game_data)
 {
-	mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.character_img.img_ptr);
-	mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.collectible_img.img_ptr);
-	mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.exit_img.img_ptr);
-	mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.wall_img.img_ptr);
-	mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.floor_img.img_ptr);
+	if (game_data->images_data.character_img.img_ptr != NULL)
+		mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.character_img.img_ptr);
+	if (game_data->images_data.collectible_img.img_ptr != NULL)
+		mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.collectible_img.img_ptr);
+	if (game_data->images_data.exit_img.img_ptr != NULL)
+		mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.exit_img.img_ptr);
+	if (game_data->images_data.wall_img.img_ptr != NULL)
+		mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.wall_img.img_ptr);
+	if (game_data->images_data.floor_img.img_ptr != NULL)
+		mlx_destroy_image(game_data->mlx_data.mlx_ptr, game_data->images_data.floor_img.img_ptr);
 }
 
 t_move_status	close_game(t_game_data *game_data)
 {
-	ft_printf("You made %d moves.\n", game_data->character_data.moves_counter);
+	display_end_message(game_data);
 	destroy_game_images(game_data);
 	mlx_destroy_window(game_data->mlx_data.mlx_ptr, game_data->mlx_data.win_ptr);
-	mlx_destroy_display(game_data->mlx_data.mlx_ptr);
+	if (game_data->mlx_data.mlx_ptr != NULL)
+	{
+		mlx_destroy_display(game_data->mlx_data.mlx_ptr);
+		free(game_data->mlx_data.mlx_ptr);
+	}
 	ft_free_and_null(game_data->map_data.map_array);
-	free(game_data->mlx_data.mlx_ptr);
 	exit(EXIT_SUCCESS);
 }
