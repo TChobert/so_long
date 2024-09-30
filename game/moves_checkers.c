e1cb5fb47f047a016f3072c3439cf45a8f5f15da
Thomas Chobert <tchobert@made-f0Dr6s1.clusters.42paris.fr>
2024-09-30 20:10:08 +0200

    30/09

--- game/moves_checkers.c
+++ game/moves_checkers.c
@@ -6,32 +6,41 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:31:42 by tchobert          #+#    #+#             */
-/*   Updated: 2024/09/30 19:30:47 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/09/30 19:46:17 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-t_move_status	check_exit_move(t_game_data *game_data, unsigned int *character_new_coords)
+t_move_status	check_exit_move(t_game_data *game_data,
+									unsigned int *character_new_coords)
 {
-	if (game_data->map_data.map_array[character_new_coords[0]][character_new_coords[1]] == EXIT_ITEM && game_data->map_data.items_values.collectibles_number == 0)
+	if (game_data->map_data.map_array[character_new_coords[0]]
+			[character_new_coords[1]] == EXIT_ITEM
+			&& game_data->map_data.items_values.collectibles_number == 0)
 	{
 		game_data->character_data.moves_counter+= 1;
 		game_data->map_data.items_values.exit_number -= 1;
 		close_game(game_data);
 	}
-	else if (game_data->map_data.map_array[character_new_coords[0]][character_new_coords[1]] == EXIT_ITEM && game_data->map_data.items_values.collectibles_number != 0)
+	else if (game_data->map_data.map_array[character_new_coords
+				[0]][character_new_coords[1]] == EXIT_ITEM
+				&& game_data->map_data.items_values.collectibles_number != 0)
 		game_data->character_data.is_on_exit = true;
 	else if (game_data->character_data.is_on_exit == true &&
-		game_data->map_data.map_array[character_new_coords[0]][character_new_coords[1]] != EXIT_ITEM)
+		game_data->map_data.map_array
+					[character_new_coords[0]][character_new_coords[1]]
+					!= EXIT_ITEM)
 	{
 		game_data->character_data.is_on_exit = false;
 	}
 	return (VALID_MOVE);
 }
-t_move_status	check_move_status(t_game_data *game_data, unsigned int *character_new_coords)
+t_move_status	check_move_status(t_game_data *game_data,
+									unsigned int *character_new_coords)
 {
-	if (game_data->map_data.map_array[character_new_coords[0]][character_new_coords[1]] == WALL_ITEM)
+	if (game_data->map_data.map_array[character_new_coords[0]]
+					[character_new_coords[1]] == WALL_ITEM)
 		return (INVALID_MOVE);
 	return (check_exit_move(game_data, character_new_coords));
 }
--- game/update_and_print_character_position.c
+++ game/update_and_print_character_position.c
@@ -6,7 +6,7 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:35:00 by tchobert          #+#    #+#             */
-/*   Updated: 2024/09/30 19:06:45 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/09/30 20:06:48 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,27 +44,66 @@ static void	print_character_on_exit(t_game_data *game_data, unsigned int *old_ch
 				old_character_coords[0], old_character_coords[1]);
 }
 
-static void	print_character_position(t_game_data *game_data,
-										unsigned int *old_character_coords)
+static void print_character_position(t_game_data *game_data,
+                                      unsigned int *old_character_coords)
 {
-	if (game_data->character_data.is_on_exit == true)
-	{
-		print_character_on_exit(game_data, old_character_coords);
-	}
-	else
-	{
-		draw_image(game_data, &game_data->images_data.floor_img,
-					old_character_coords[0], old_character_coords[1]);
-		if (game_data->character_data.is_right == true)
-			draw_image(game_data, &game_data->images_data.character_right_img,
-							game_data->character_data.character_coord[0],
-							game_data->character_data.character_coord[1]);
-		else
-		draw_image(game_data, &game_data->images_data.character_img,
-						game_data->character_data.character_coord[0],
-						game_data->character_data.character_coord[1]);
-	}
+    // Vérifier si le personnage est sur la sortie
+    if (game_data->character_data.is_on_exit == true)
+    {
+        print_character_on_exit(game_data, old_character_coords);
+    }
+    else
+    {
+        // Si l'ancienne position était sur la sortie, dessiner l'image de sortie
+        if (game_data->map_data.map_array[old_character_coords[0]][old_character_coords[1]] == EXIT_ITEM)
+        {
+            // Dessiner l'image de sortie
+            draw_image(game_data, &game_data->images_data.exit_img, old_character_coords[0], old_character_coords[1]);
+        }
+        else
+        {
+            // Dessiner l'image du sol si on n'est pas sur la sortie
+            draw_image(game_data, &game_data->images_data.floor_img,
+                       old_character_coords[0], old_character_coords[1]);
+        }
+
+        // Dessiner l'image du personnage
+        if (game_data->character_data.is_right == true)
+            draw_image(game_data, &game_data->images_data.character_right_img,
+                       game_data->character_data.character_coord[0],
+                       game_data->character_data.character_coord[1]);
+        else
+            draw_image(game_data, &game_data->images_data.character_img,
+                       game_data->character_data.character_coord[0],
+                       game_data->character_data.character_coord[1]);
+    }
 }
+
+// static void	print_character_position(t_game_data *game_data,
+// 										unsigned int *old_character_coords)
+// {
+// 	if (game_data->character_data.is_on_exit == true)
+// 	{
+// 		print_character_on_exit(game_data, old_character_coords);
+// 	}
+// 	else if (game_data->map_data.map_array[old_character_coords[0]][old_character_coords[1]] == EXIT_ITEM)
+// 	{
+// 		draw_image(game_data, &game_data->images_data.exit_img, old_character_coords[0], old_character_coords[1]);
+// 	}
+// 	else
+// 	{
+// 		draw_image(game_data, &game_data->images_data.floor_img,
+// 					old_character_coords[0], old_character_coords[1]);
+// 	}
+// 	if (game_data->character_data.is_right == true)
+// 		draw_image(game_data, &game_data->images_data.character_right_img,
+// 							game_data->character_data.character_coord[0],
+// 							game_data->character_data.character_coord[1]);
+// 	else
+// 		draw_image(game_data, &game_data->images_data.character_img,
+// 						game_data->character_data.character_coord[0],
+// 						game_data->character_data.character_coord[1]);
+// }
 
 void	update_and_print_character_position(t_game_data *game_data,
 												unsigned int *character_new_coords)
